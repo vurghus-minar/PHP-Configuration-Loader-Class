7c4995bfe40b012dc6e0a027ec8c202254c6ba14
vurghus-minar <vurghus.minar@outlook.com>
2019-06-12 17:48:33 +0400

    major clean and refactoring

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -21,24 +21,6 @@ class ConfigTest extends TestCase {
     }
 
 
-    
-    public function testYamlIsValid(){
-    	
-
-        $config_file_true = dirname(__DIR__) . '/examples/configs/config.yaml';
-    	$config_true = New \Am\Config\Config($config_file_true);
-        $is_yaml_valid_method_true = $this->invokeMethod($config_true,'is_yaml_valid', array($config_file_true));
-        $this->assertTrue($is_yaml_valid_method_true);
-
-
-        $config_file_false = dirname(__DIR__) . '/examples/configs/config-contains-mistakes.yaml';
-        $config_false = New \Am\Config\Config($config_file_false);
-        $is_yaml_valid_method_false = $this->invokeMethod($config_false,'is_yaml_valid', array($config_file_false));
-        $this->assertFalse($is_yaml_valid_method_false);
-
-    }
-
-
 
     public function testIfConfigArrayIsAnAcceptableArgument()
     {
@@ -87,29 +69,6 @@ class ConfigTest extends TestCase {
     }
 
 
-    public function testInvalidSrcErrorMessage(){
-
-        $config_array = array(
-            'root_path' => $_SERVER['DOCUMENT_ROOT'],
-            'base_url' => '/',
-            'directories' => array(
-                'controllers' => 'controllers',
-                'models' => 'models',
-                'views' => 'views',
-                'core' => 'core'        
-            )
-        );
-
-        $src = 3;
-
-        $config = New \Am\Config\Config($config_array, $src, true);
-
-        $output_error = ob_get_contents();
-        $this->assertEquals("<div style='border:1px solid #EEE;background:#f9f6d6; padding:10px 10px 0px;margin:10px;font-family:sans-serif;'><h4 style='color:#E43137;margin:0px;'>Error:</h4><p>\$src can only be 1 or 0 (0 is the default argument)</p></div>", $output_error);
-
-    }
-
-
     /**
      * @dataProvider configArray
      * */
@@ -120,31 +79,6 @@ class ConfigTest extends TestCase {
         $config = New \Am\Config\Config($config_array, $src, true);
 
         $this->assertEquals($config->test_number, 10);
-
-    }
-
-    public function testErrorMessage(){
-
-        $config_array = array(
-            'root_path' => $_SERVER['DOCUMENT_ROOT'],
-            'base_url' => '/',
-            'directories' => array(
-                'controllers' => 'controllers',
-                'models' => 'models',
-                'views' => 'views',
-                'core' => 'core'        
-            ),
-            'debug' => true
-        );
-
-        $config = New \Am\Config\Config($config_array);
-
-        $errStr = "This is an Error!";
-        $show_errmsg_method = $this->invokeMethod($config, 'errmsg', array($errStr));
-
-        $expected = "<div style='border:1px solid #EEE;background:#f9f6d6; padding:10px 10px 0px;margin:10px;font-family:sans-serif;'><h4 style='color:#E43137;margin:0px;'>Error:</h4><p>$errStr</p></div>";
-
-        $this->AssertEquals($expected, $show_errmsg_method);
 
     }
 
